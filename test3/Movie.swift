2ff8839e78bc9624c191c6e861460116e699fcb2
AbdigaliAlmagul <98630900+AAlmagul@users.noreply.github.com>
2024-11-08 23:22:51 +0500

    Create Movie.swift

---
+++ test3/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  movie segue
+//
+//  Created by Алмагуль Абдыгали on 07.11.2024.
+//
+
+import UIKit
+
+struct Movie {
+    let title: String
+    let description: String
+    let image: UIImage
+    let year: String
+    let genre: String
+    var isFavorite: Bool 
+}
